4b49305f675178ed410002128e3e01bbdc2444ee
s-arya-08 <sanskararya090@gmail.com>
2025-09-05 00:58:09 +0530

    Testing for container

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Azure Container Instances via Jenkins Pipeline!"
+    return "Hello from Azure Container Instances via Jenkins Pipeline! Updated on 05 Septemeber 2025"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
